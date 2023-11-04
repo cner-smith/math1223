3e5c1367d48ed0ab5a1abda84033b8e9ac83da5e
conner <connersmith+cloudresume@gmail.com>
2023-11-04 13:58:17 -0500

    Added functionality to print out highest secondary and count # and type of gems used

--- source/main.py
+++ source/main.py
@@ -5,16 +5,15 @@ import logging
 import datetime
 import os
 
-from typing import Union
 from datetime import timedelta
-
+from requests import Response
 logging.basicConfig(level=logging.ERROR)
 
 # Initialize a counter for the number of files written
 files_written_counter = 0
 
 
-def get_source_id(code: str, fight_id: str, name: str) -> Union[str, None]:
+def get_source_id(code: str, fight_id: str, name: str) -> str:
     query = GET_SOURCE_ID_QUERY.format(code=code, fightID=fight_id)
     response = oauth2_client.make_request(GRAPHQL_ENDPOINT, query)
     
@@ -27,7 +26,8 @@ def get_source_id(code: str, fight_id: str, name: str) -> Union[str, None]:
                     if entry['name'] == name:
                         return entry['id']
                 logging.error(f"No matching player found for {name}")
-                return None  # Return None if player not found
+                # TODO: need to probably find/make an exception for data retrieve fails
+                raise  # Raise Error if data retrieval fails
             else:
                 logging.error(f"No 'composition' data in the response.")
         except Exception as e:
@@ -37,7 +37,7 @@ def get_source_id(code: str, fight_id: str, name: str) -> Union[str, None]:
     else:
         logging.error(f"Request failed with status code: {response.status_code}")
 
-    return None  # Return None if data retrieval fails
+    raise  # Return None if data retrieval fails
 
 
 def export_player_data(fight_id: str, code: str, source_id: str) -> None:
@@ -84,6 +84,36 @@ def modify_combatant_info_data(combatant_info: str) -> list[dict[str, str]]:
         modified_combatant_info.append({'info_key': stat_key, 'info_value': value_to_add})
     return modified_combatant_info
 
+def get_highest_secondary_stat(stats_data: dict) -> str:
+    # Define the stat keys we're interested in
+    stat_keys = ['Mastery', 'Haste', 'Crit', 'Versatility']
+    
+    # Extract the relevant stats and find the highest one
+    highest_stat = max(stat_keys, key=lambda stat: stats_data.get(stat, {'min': 0})['min'])
+    
+    return highest_stat
+
+def process_gems(gear_data: list[dict[str, any]]) -> list[dict[str, [str, int]]]:
+    # Create a dictionary to hold the count of each gem by its ID
+    gem_count = {}
+    # Iterate through each item in the gear
+    for item in gear_data:
+        # Check if there are gems in the item
+        if 'gems' in item:
+            for gem in item['gems']:
+                gem_id = gem['id']
+                # Increment the count for each gem ID
+                gem_count[gem_id] = gem_count.get(gem_id, 0) + 1
+
+    # Convert gem IDs to gem names using the GEM_ID_TO_NAME mapping
+    gem_info = [{'gem_name': GEM_ID_TO_NAME.get(gem_id, f"Unknown Gem ID {gem_id}"), 'gem_count': count}
+                for gem_id, count in gem_count.items()]
+    
+    return gem_info
+
+def id_to_name(gem_id: int) -> str:
+    return GEM_ID_TO_NAME.get(gem_id, "Unknown Gem")
+
 
 def process_data_and_export(name: str, data: str) -> None:
     global files_written_counter
@@ -95,13 +125,21 @@ def process_data_and_export(name: str, data: str) -> None:
 
     modified_healing_data = modify_healing_done_data(data.get('healingDone', []))
     modified_combatant_info_data = modify_combatant_info_data(data.get('combatantInfo', {}))
+    highest_secondary = get_highest_secondary_stat(data.get('combatantInfo', {}).get('stats', {}))
+    gear_data = data.get('combatantInfo', {}).get('gear', [])
+    gem_info_list = process_gems(gear_data)
 
     # Check if csv-output file exists, if not create it
     os.makedirs("csv-output", exist_ok=True)
 
     # Write the modified data to a new CSV file named after the player's name
     with open(f"csv-output/{name}_modified.csv", "w", newline="") as f:
-        writer = csv.DictWriter(f, fieldnames=['total_time', 'healers_count', 'stats', 'total', 'ability', 'healing_done_total'])
+        fieldnames = [
+            'total_time', 'healers_count', 'stats', 'stats_total',
+            'highest_secondary', 'ability_name', 'healing_done_total',
+            'gem_name', 'gem_count'
+        ]
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
         writer.writeheader()
 
         # Concurrently write combatant info data and healing data
@@ -111,8 +149,11 @@ def process_data_and_export(name: str, data: str) -> None:
                 'total_time': total_time if i == 0 else None,  # Write only once
                 'healers_count': healers_count if i == 0 else None,  # Write only once
                 'stats': modified_combatant_info_data[i]['info_key'] if i < len(modified_combatant_info_data) else None,
-                'total': modified_combatant_info_data[i]['info_value'] if i < len(modified_combatant_info_data) else None,
-                'ability': modified_healing_data[i]['ability'] if i < len(modified_healing_data) else None,
+                'stats_total': modified_combatant_info_data[i]['info_value'] if i < len(modified_combatant_info_data) else None,
+                'highest_secondary': highest_secondary if i == 0 else None,  # Include the highest secondary stat in the first row
+                'gem_name': gem_info_list[i]['gem_name'] if i < len(gem_info_list) else None,
+                'gem_count': gem_info_list[i]['gem_count'] if i < len(gem_info_list) else None,
+                'ability_name': modified_healing_data[i]['ability'] if i < len(modified_healing_data) else None,
                 'healing_done_total': modified_healing_data[i]['healing_amount'] if i < len(modified_healing_data) else None
             }
             writer.writerow(row_data)
@@ -122,7 +163,7 @@ def process_data_and_export(name: str, data: str) -> None:
 
 
 class OAuth2Client:
-    def __init__(self, client_id: str, client_secret: str, authorization_url: str, access_token_url: str):
+    def __init__(self, client_id: str, client_secret: str, authorization_url: str, access_token_url: str) -> None:
         self.client_id = client_id
         self.client_secret = client_secret
         self.authorization_url = authorization_url
@@ -131,7 +172,7 @@ class OAuth2Client:
         self.token_expiry = None
         self.get_access_token()
 
-    def get_access_token(self) -> Union[str, None]:
+    def get_access_token(self) -> str:
         if hasattr(self, 'access_token') and self.access_token and hasattr(self, 'token_expiry') and self.token_expiry and datetime.datetime.now() < self.token_expiry:
             return self.access_token
         
@@ -148,9 +189,9 @@ class OAuth2Client:
             return self.access_token
         else:
             logging.error(f"Failed to obtain access token.")
-            return None
+            raise
 
-    def make_request(self, url: str, query: str = ''):
+    def make_request(self, url: str, query: str = '') -> Response:
         headers = {
             "Authorization": f"Bearer {self.access_token}"
         }
@@ -196,6 +237,33 @@ query request {{
     }}
 }}
 """
+GEM_ID_TO_NAME = {
+    192973: "Forceful Nozdorite",
+    192919: "Crafty Alexstraszite",
+    192955: "Quick Ysemerald",
+    192961: "Keen Neltharite",
+    192935: "Energized Malygite",
+    192976: "Puissant Nozdorite",
+    192938: "Zen Malygite",
+    192967: "Fractured Neltharite",
+    192922: "Sensei's Alexstraszite",
+    192948: "Keen Ysemerald",
+    192970: "Jagged Nozdorite",
+    192945: "Crafty Ysemerald",
+    192928: "Deadly Alexstraszite",
+    192932: "Radiant Malygite",
+    192958: "Sensei's Neltharite",
+    192979: "Steady Nozdorite",
+    192964: "Zen Neltharite",
+    192942: "Stormy Malygite",
+    192952: "Energized Ysemerald",
+    192925: "Radiant Alexstraszite",
+    192985: "Fierce Illimited Diamond",
+    192988: "Skillful Illimited Diamond",
+    192982: "Inscribed Illimited Diamond",
+    192991: "Resplendent Illimited Diamond",
+    # ... add other mappings as needed
+}
 
 # TODO: consider transition to .env file instead of config.json file
 with open('config.json', 'r') as f:
@@ -210,7 +278,7 @@ oauth2_client = OAuth2Client(
 )
 
 
-def main():
+def main() -> None:
 
     access_token = oauth2_client.get_access_token()
 
@@ -219,13 +287,11 @@ def main():
         return
 
     response = oauth2_client.make_request(GRAPHQL_ENDPOINT, GET_TOP_PRIESTS_QUERY)
-
     if response.status_code != 200:
         logging.error(f"Request for top priests data failed with status code: {response.status_code}")
         return
 
     top_priests_response = response.json()
-    
     if 'data' in top_priests_response:
         priest_rankings = top_priests_response['data']['worldData']['encounter']['characterRankings']['rankings']
 
